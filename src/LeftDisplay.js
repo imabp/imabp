3ecf4e8d7bfef39372aac27205882d047ed63597
imabp <abir.pal899@gmail.com>
2020-08-11 15:39:12 +0530

    Content BrushingUp 😎👨‍💻

--- src/LeftDisplay.js
+++ src/LeftDisplay.js
@@ -1,12 +1,28 @@
 import React, {Component} from 'react';
-import favicon from './images/favicon.svg'
 import {motion} from "framer-motion"
 
+//importing images
+import favicon from './images/favicon.svg'
 import mspbadge from './images/msp_badge.svg'
-// Update: Avatar not displayed. 
-class Avatar extends Component{
-    render(){
-        
+ 
+class LeftDisplay extends Component{
+    render()
+    {
+  
+     return ( 
+               <div className="LeftPanel">
+               
+                  <Avatar/><br/>
+                  <UserName details={{"fn":this.props.details['firstname'],"ln":this.props.details['lastname']}} />
+                  <Description details="Computer Science Undergraduate"/>
+                 <br/>
+                 <p style={{textAlign:"center"}}> I <span> ❤️</span> to build applications that becomes a force for good.</p>
+              </div>
+              )
+    }
+  }  
+//avatar imabp
+function Avatar (props){       
         return(
             <motion.div className="avatar-class" animate={{
                 
@@ -16,43 +32,29 @@ class Avatar extends Component{
                 <img alt="Avatar"  src={favicon}/>
             </motion.div>
         )
-    }
+  
 }
-
-class UserName extends Component{
-    render(){
-        const fname=this.props.details["fn"];
-        const lname=this.props.details["ln"];
-        return(
+//name display
+function UserName (props){
+    const fname=props.details["fn"];
+    const lname=props.details["ln"];
+    return(     
            <div className="username" style={{textAlign:"center"}}>
              
               <span><h1>{fname} {lname}</h1></span>
            </div>
         )
-    }
+   
 }
-class Description extends Component{
-    render(){
-        return <div className="desc">
-            <p className="CODEdecorate" style={{textAlign:"center"}}>{this.props.details}<br/><br/>
+//description display
+function Description (props){
+    return(
+            <div className="desc">
+            <p className="CODEdecorate" style={{textAlign:"center"}}>{props.details}<br/><br/>
             <img alt="msp" width="70%" src={mspbadge}/><br/></p>
           
         </div>
-    }
+    )
 }
-class LeftDisplay extends Component{
-  render()
-  {
 
-   return ( 
-             <div className="LeftPanel">
-             
-                <Avatar/><br/>
-                <UserName details={{"fn":this.props.details['firstname'],"ln":this.props.details['lastname']}} />
-                <Description details="Computer Science Undergraduate"/>
-              
-            </div>
-            )
-  }
-}  
 export default LeftDisplay;
